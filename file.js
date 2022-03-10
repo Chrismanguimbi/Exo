b659c0f89a714024a8e0caf8c680a3efa9373a24
NGUIMBI CHRISMA <chrisma@macbook-pro-de-nguimbi.home>
2022-03-10 22:12:01 +0100

    premier commit

---
+++ file.js
@@ -0,0 +1,20 @@
+let age = 38;
+let myName = "Nguimbi Chrisma";
+let student = false;
+let school = "3i School";
+
+console.log(typeof(age));
+console.log(typeof(myName));
+console.log(typeof(student));
+console.log(typeof(school));
+
+console.log(age);
+console.log(myName);
+console.log(student);
+console.log(school);
+
+age = age++;
+student = true;
+
+console.log(age);
+console.log(student);
---
+++ script.js
@@ -0,0 +1,11 @@
+const temperature = 26;
+
+if(temperature >= 10 && temperature <=20){
+    console.log("Mettre un pull");
+}
+if(temperature < 10){
+    console.log("Mettre un manteau");
+}
+if(temperature > 20){
+    console.log("Mettre seulement un tee-shirt");
+}
